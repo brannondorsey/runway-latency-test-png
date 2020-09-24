1a5145521ae1e1723c98d4c7e67fa3f5e1a1004c
Brannon Dorsey <brannon@brannondorsey.com>
2020-09-24 12:48:44 -0400

    Crash loop test

--- runway_model.py
+++ runway_model.py
@@ -1,8 +1,11 @@
 import runway
+import os, signal
 from runway.data_types import image
 
 @runway.command(name='latency_test', inputs={ 'image': image() }, outputs={ 'image': image() })
 def latency_test(model, args):
+    # Test a crash loop
+    os.kill(os.getpid(), signal.SIGKILL)
     return { 'image': args['image'] }
 
 if __name__ == '__main__':
